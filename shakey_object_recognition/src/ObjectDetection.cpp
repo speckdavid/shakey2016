99bca50aef98f8f88068688cc263d3cda224ed15
David Speck <david.speck@pluto.uni-freiburg.de>
2016-03-15 14:18:50 +0100

    now restore points not belonging to the cluster of an detected object

--- shakey_object_recognition/src/ObjectDetection.cpp
+++ shakey_object_recognition/src/ObjectDetection.cpp
@@ -53,6 +53,8 @@ class ObjectDetection {
 	float _boxAngleDelta;
 	float _wedgeAngleDelta;
 	float _groundDistDelta;
+	float _boxPointDensity;
+	float _wedgePointDensity;
 	float _boxDistDelta;
 	float _wedgeDistDelta;
 	int _minClusterPoints;
@@ -74,7 +76,7 @@ class ObjectDetection {
 public:
 	ObjectDetection() {
 		load_params();
-		sub = nh.subscribe("/head_mount_kinect/depth/points", 1,
+		sub = nh.subscribe("/head_mount_kinect/depth_registered/points", 1,
 				&ObjectDetection::normal_spin, this);
 		service = nh.advertiseService("/detectObject", &ObjectDetection::detect,
 				this);
@@ -181,19 +183,26 @@ public:
 		_boxAngleDelta = d_tmp + 0.0f;
 		nh.getParam("ObjectDetection/boxDistDelta", d_tmp);
 		_boxDistDelta = d_tmp + 0.0f;
+		nh.getParam("ObjectDetection/boxPointDensity", d_tmp);
+		_boxPointDensity = d_tmp + 0.0f;
 		temp.str("");
 		temp.clear();
-		temp << _boxAngleDelta << "deg and max.z > " << _boxDistDelta;
-		ROS_INFO("Box Angle Delta: %s", temp.str().c_str());
+		temp << "angleDelta = " << _boxAngleDelta << "deg and max.z > "
+				<< _boxDistDelta << " and pointDensity > " << _boxPointDensity;
+		ROS_INFO("Box : %s", temp.str().c_str());
 
 		nh.getParam("ObjectDetection/wedgeAngleDelta", d_tmp);
 		_wedgeAngleDelta = d_tmp + 0.0f;
 		nh.getParam("ObjectDetection/wedgeDistDelta", d_tmp);
 		_wedgeDistDelta = d_tmp + 0.0f;
+		nh.getParam("ObjectDetection/wedgePointDensity", d_tmp);
+		_wedgePointDensity = d_tmp + 0.0f;
 		temp.str("");
 		temp.clear();
-		temp << _wedgeAngleDelta << "deg and max.z > " << _wedgeDistDelta;
-		ROS_INFO("Wedge Delta: %s", temp.str().c_str());
+		temp << "angleDelta = " << _wedgeAngleDelta << "deg and max.z > "
+				<< _wedgeDistDelta << " and pointDensity > "
+				<< _wedgePointDensity;
+		ROS_INFO("Wedge: %s", temp.str().c_str());
 
 		nh.getParam("ObjectDetection/world_frame", _worldFrame);
 		ROS_INFO("Using world_frame = %s", _worldFrame.c_str());
@@ -232,12 +241,13 @@ public:
 		// Create the filtering object
 		pcl::ExtractIndices<pcl::PointXYZ> extract;
 
-        pcl::VoxelGrid< pcl::PointXYZ > sor;
-        sor.setInputCloud(cloud_out);
-        sor.setLeafSize(0.015f, 0.015f, 0.015f);
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_down(new pcl::PointCloud<pcl::PointXYZ>());
-        sor.filter(*cloud_down);
-        cloud_out = cloud_down;
+		pcl::VoxelGrid<pcl::PointXYZ> sor;
+		sor.setInputCloud(cloud_out);
+		sor.setLeafSize(0.015f, 0.015f, 0.015f);
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_down(
+				new pcl::PointCloud<pcl::PointXYZ>());
+		sor.filter(*cloud_down);
+		cloud_out = cloud_down;
 
 		int i = 0, nr_points = (int) cloud_out->points.size();
 		// While 5% of the original cloud is still there
@@ -325,6 +335,9 @@ public:
 
 			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(
 					new pcl::PointCloud<pcl::PointXYZ>);
+			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_restore(
+					new pcl::PointCloud<pcl::PointXYZ>());
+			bool found_obj = false;
 			for (int k = 0; k < cluster_indices.size(); k++) {
 				pcl::PointIndices::Ptr cur_ind(
 						new pcl::PointIndices(cluster_indices[k]));
@@ -332,12 +345,6 @@ public:
 				extract.setIndices(cur_ind);
 				extract.setNegative(false);
 				extract.filter(*cloud_cluster);
-
-				temp.str("");
-				temp.clear();
-				temp << " " << k << "-th Cluster with "
-						<< cluster_indices[k].indices.size() << " Points.";
-				ROS_INFO("%s", temp.str().c_str());
 
 				// debug for cloud
 				pcl::toROSMsg(*cloud_cluster, out);
@@ -350,26 +357,48 @@ public:
 				float width_p, length_p;
 				Eigen::Matrix3f eigenVectors = eigenVectorsAndSize(minBox,
 						&width_p, &length_p);
+				float point_density = cluster_indices[k].indices.size()
+						/ (width_p * length_p);
+
+				temp.str("");
+				temp.clear();
+				temp << " " << k << "-th Cluster with "
+						<< cluster_indices[k].indices.size() << " Points and "
+						<< point_density << " Points/sqrmeters.";
+				ROS_INFO("%s", temp.str().c_str());
+
 				if (width_p < _minXDistance || length_p < _minYDistance) {
 					ROS_INFO("Not required object size.");
+					*cloud_restore += *cloud_cluster;
 					continue;
 				}
 
 				shakey_object_recognition::PushableObject obj;
 				// parallel plane to ground plane (top plane of box)
 				if (ang < _boxAngleDelta && max.z() > _boxDistDelta) {
+					if (point_density <= _boxPointDensity) {
+						ROS_INFO("Not required points per square meter.");
+						*cloud_restore += *cloud_cluster;
+						continue;
+					}
 					ROS_INFO(" => BOX TOP PLANE");
 					obj.obj_type = Box;
 				}
 				// crooked plane (wedge)
 				else if (std::abs(45 - ang) < _wedgeAngleDelta
 						&& max.z() > _wedgeDistDelta) {
+					if (point_density <= _wedgePointDensity) {
+						ROS_INFO("Not required points per square meter.");
+						*cloud_restore += *cloud_cluster;
+						continue;
+					}
 					ROS_INFO(" => WEDGE CROOKED PLANE");
 					obj.obj_type = Wedge;
-				}
-				else {
+				} else {
+					*cloud_restore += *cloud_cluster;
 					continue;
 				}
+				found_obj = true;
 				obj.frame_id = _worldFrame;
 				// from middle point (not from front)
 				obj.dist_to_obj = _offset;
@@ -406,178 +435,193 @@ public:
 
 				// Add to return
 				objects.push_back(obj);
+			}
+			if (found_obj)
+				*cloud_out += *cloud_restore;
+			i++;
 		}
-		i++;
+		std::ostringstream temp;
+		temp << "Detections: [ ";
+		for (int i = 0; i < objects.size(); i++) {
+			if (objects.at(i).obj_type == Box)
+				temp << "Box ";
+			if (objects.at(i).obj_type == Wedge)
+				temp << "Wedge ";
+		}
+		temp << "]";
+		ROS_INFO("%s", temp.str().c_str());
+		std::cerr
+				<< "--------------------------------------------------------------"
+				<< std::endl;
+		std::cerr
+				<< "--------------------------------------------------------------"
+				<< std::endl;
+		return objects;
 	}
-	std::ostringstream temp;temp << "Detections: [ ";for (int i = 0;i < objects.size();i++) {
-				if (objects.at(i).obj_type == Box) temp << "Box ";
-				if (objects.at(i).obj_type == Wedge) temp << "Wedge ";
-			}temp << "]";ROS_INFO("%s", temp.str().c_str());std::cerr << "--------------------------------------------------------------" << std::endl;std::cerr << "--------------------------------------------------------------" << std::endl;return objects;
-		}
 
 private:
-  pcl::PointCloud<pcl::PointXYZ> transformed_cloud(const sensor_msgs::PointCloud2ConstPtr& input)  
-  {
-	tf_error = false;
-	pcl::PointCloud<pcl::PointXYZ> cloud_in;
-	pcl::PointCloud<pcl::PointXYZ> cloud_out;
-    pcl::fromROSMsg (*input, cloud_in);
-	tf::StampedTransform transform;
-    try
-    {
-	  listener.waitForTransform(_worldFrame, input->header.frame_id,
-	    input->header.stamp , ros::Duration(0.5));
-      listener.lookupTransform(_worldFrame, input->header.frame_id,
-        input->header.stamp, transform);
-    }
-    catch (tf::TransformException &ex) {
-          tf_error = true;
-          ros::Duration(0.5).sleep();
-          		ROS_ERROR("%s", ex.what()); }
-	pcl_ros::transformPointCloud(cloud_in, cloud_out, transform);
-	cloud_out.header.frame_id = _worldFrame;
-	return cloud_out;
-}
-
-float angle(Eigen::Vector3f v1, Eigen::Vector3f v2) {
-	float cos_sim = v1.dot(v2) / v1.norm() * v2.norm();
-	return acos(cos_sim) / M_PI * 180;
-}
-
-std::vector<Eigen::Vector3f> minimal2DBoundingBox(
-		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_p,
-		pcl::ModelCoefficients::Ptr coefficients) {
-	std::vector<Eigen::Vector3f> plane_bbx;
-
-	// store the top plane parameters
-	Eigen::Vector3f plane_normal;
-	plane_normal.x() = coefficients->values[0];
-	plane_normal.y() = coefficients->values[1];
-	plane_normal.z() = coefficients->values[2];
-	// compute an orthogonal normal to the plane normal
-	Eigen::Vector3f v = plane_normal.unitOrthogonal();
-	// take the cross product of the two normals to get
-	// a thirds normal, on the plane
-	Eigen::Vector3f u = plane_normal.cross(v);
-
-	// project the 3D point onto a 2D plane
-	std::vector<cv::Point2f> points;
-	// choose a point on the plane
-	Eigen::Vector3f p0(cloud_p->points[0].x, cloud_p->points[0].y,
-			cloud_p->points[0].z);
-	for (unsigned int i = 0; i < cloud_p->points.size(); i++) {
-		Eigen::Vector3f p3d(cloud_p->points[i].x, cloud_p->points[i].y,
-				cloud_p->points[i].z);
-
-		// subtract all 3D points with a point in the plane
-		// this will move the origin of the 3D coordinate system
-		// onto the plane
-		p3d = p3d - p0;
-
-		cv::Point2f p2d;
-		p2d.x = p3d.dot(u);
-		p2d.y = p3d.dot(v);
-		points.push_back(p2d);
+	pcl::PointCloud<pcl::PointXYZ> transformed_cloud(
+			const sensor_msgs::PointCloud2ConstPtr& input) {
+		tf_error = false;
+		pcl::PointCloud<pcl::PointXYZ> cloud_in;
+		pcl::PointCloud<pcl::PointXYZ> cloud_out;
+		pcl::fromROSMsg(*input, cloud_in);
+		tf::StampedTransform transform;
+		try {
+			listener.waitForTransform(_worldFrame, input->header.frame_id,
+					input->header.stamp, ros::Duration(0.5));
+			listener.lookupTransform(_worldFrame, input->header.frame_id,
+					input->header.stamp, transform);
+		} catch (tf::TransformException &ex) {
+			tf_error = true;
+			ros::Duration(0.5).sleep();
+			ROS_ERROR("%s", ex.what());
+		}
+		pcl_ros::transformPointCloud(cloud_in, cloud_out, transform);
+		cloud_out.header.frame_id = _worldFrame;
+		return cloud_out;
 	}
 
-	cv::Mat points_mat(points);
-	cv::RotatedRect rrect = cv::minAreaRect(points_mat);
-	cv::Point2f rrPts[4];
-	rrect.points(rrPts);
-
-	//store the table top bounding points in a vector
-	for (unsigned int i = 0; i < 4; i++) {
-		Eigen::Vector3f pbbx(rrPts[i].x * u + rrPts[i].y * v + p0);
-		plane_bbx.push_back(pbbx);
+	float angle(Eigen::Vector3f v1, Eigen::Vector3f v2) {
+		float cos_sim = v1.dot(v2) / v1.norm() * v2.norm();
+		return acos(cos_sim) / M_PI * 180;
 	}
-	Eigen::Vector3f center(rrect.center.x * u + rrect.center.y * v + p0);
-	plane_bbx.push_back(center);
 
-	// 0-3: 4 corner points, 4: mean
-	return plane_bbx;
-}
+	std::vector<Eigen::Vector3f> minimal2DBoundingBox(
+			pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_p,
+			pcl::ModelCoefficients::Ptr coefficients) {
+		std::vector<Eigen::Vector3f> plane_bbx;
+
+		// store the top plane parameters
+		Eigen::Vector3f plane_normal;
+		plane_normal.x() = coefficients->values[0];
+		plane_normal.y() = coefficients->values[1];
+		plane_normal.z() = coefficients->values[2];
+		// compute an orthogonal normal to the plane normal
+		Eigen::Vector3f v = plane_normal.unitOrthogonal();
+		// take the cross product of the two normals to get
+		// a thirds normal, on the plane
+		Eigen::Vector3f u = plane_normal.cross(v);
+
+		// project the 3D point onto a 2D plane
+		std::vector<cv::Point2f> points;
+		// choose a point on the plane
+		Eigen::Vector3f p0(cloud_p->points[0].x, cloud_p->points[0].y,
+				cloud_p->points[0].z);
+		for (unsigned int i = 0; i < cloud_p->points.size(); i++) {
+			Eigen::Vector3f p3d(cloud_p->points[i].x, cloud_p->points[i].y,
+					cloud_p->points[i].z);
+
+			// subtract all 3D points with a point in the plane
+			// this will move the origin of the 3D coordinate system
+			// onto the plane
+			p3d = p3d - p0;
+
+			cv::Point2f p2d;
+			p2d.x = p3d.dot(u);
+			p2d.y = p3d.dot(v);
+			points.push_back(p2d);
+		}
+
+		cv::Mat points_mat(points);
+		cv::RotatedRect rrect = cv::minAreaRect(points_mat);
+		cv::Point2f rrPts[4];
+		rrect.points(rrPts);
+
+		//store the table top bounding points in a vector
+		for (unsigned int i = 0; i < 4; i++) {
+			Eigen::Vector3f pbbx(rrPts[i].x * u + rrPts[i].y * v + p0);
+			plane_bbx.push_back(pbbx);
+		}
+		Eigen::Vector3f center(rrect.center.x * u + rrect.center.y * v + p0);
+		plane_bbx.push_back(center);
+
+		// 0-3: 4 corner points, 4: mean
+		return plane_bbx;
+	}
 
 // TODO: Verify order of points
-Eigen::Matrix3f eigenVectorsAndSize(std::vector<Eigen::Vector3f> plane_bbx,
-		float *width, float *length) {
-	Eigen::Matrix3f eigenVectors;
-	Eigen::Vector3f e0 = plane_bbx[0] - plane_bbx[1];
-	*width = Eigen::Vector2f(e0.x(), e0.y()).norm();
-	e0.normalize();
-	Eigen::Vector3f e1 = plane_bbx[0] - plane_bbx[3];
-	*length = Eigen::Vector2f(e1.x(), e1.y()).norm();
-	e1.normalize();
-	Eigen::Vector3f e2 = e0.cross(e1);
-	eigenVectors << e0, e1, e2;
-	return eigenVectors;
-}
+	Eigen::Matrix3f eigenVectorsAndSize(std::vector<Eigen::Vector3f> plane_bbx,
+			float *width, float *length) {
+		Eigen::Matrix3f eigenVectors;
+		Eigen::Vector3f e0 = plane_bbx[0] - plane_bbx[1];
+		*width = Eigen::Vector2f(e0.x(), e0.y()).norm();
+		e0.normalize();
+		Eigen::Vector3f e1 = plane_bbx[0] - plane_bbx[3];
+		*length = Eigen::Vector2f(e1.x(), e1.y()).norm();
+		e1.normalize();
+		Eigen::Vector3f e2 = e0.cross(e1);
+		eigenVectors << e0, e1, e2;
+		return eigenVectors;
+	}
 
 // TODO: Verify order of points
-std::vector<geometry_msgs::Pose> getPushData(
-		std::vector<Eigen::Vector3f> plane_bbx) {
-	//std::cerr << plane_bbx[0]<< "\n\n" << plane_bbx[1] << "\n\n" << plane_bbx[2] << "\n\n"<< plane_bbx[3]<< std::endl;
-	std::vector<Eigen::Vector3f> points;
-	std::vector<Eigen::Vector3f> vecs;
-	// Add all points if not both are on the floor (flat side on wedge)
-	if (plane_bbx[0](2) > 0.15 || plane_bbx[1](2) > 0.15) {
-		Eigen::Vector3f p0 = (plane_bbx[0] + plane_bbx[1]) * 0.5f;
-		points.push_back(p0);
-	}
-	if (plane_bbx[0](2) > 0.15 || plane_bbx[3](2) > 0.15) {
-		Eigen::Vector3f p1 = (plane_bbx[0] + plane_bbx[3]) * 0.5f;
-		points.push_back(p1);
-	}
-	if (plane_bbx[2](2) > 0.15 || plane_bbx[1](2) > 0.15) {
-		Eigen::Vector3f p2 = (plane_bbx[2] + plane_bbx[1]) * 0.5f;
-		points.push_back(p2);
-	}
-	if (plane_bbx[0](2) > 0.15 || plane_bbx[3](2) > 0.15) {
-		Eigen::Vector3f p3 = (plane_bbx[2] + plane_bbx[3]) * 0.5f;
-		points.push_back(p3);
+	std::vector<geometry_msgs::Pose> getPushData(
+			std::vector<Eigen::Vector3f> plane_bbx) {
+		//std::cerr << plane_bbx[0]<< "\n\n" << plane_bbx[1] << "\n\n" << plane_bbx[2] << "\n\n"<< plane_bbx[3]<< std::endl;
+		std::vector<Eigen::Vector3f> points;
+		std::vector<Eigen::Vector3f> vecs;
+		// Add all points if not both are on the floor (flat side on wedge)
+		if (plane_bbx[0](2) > 0.15 || plane_bbx[1](2) > 0.15) {
+			Eigen::Vector3f p0 = (plane_bbx[0] + plane_bbx[1]) * 0.5f;
+			points.push_back(p0);
+		}
+		if (plane_bbx[0](2) > 0.15 || plane_bbx[3](2) > 0.15) {
+			Eigen::Vector3f p1 = (plane_bbx[0] + plane_bbx[3]) * 0.5f;
+			points.push_back(p1);
+		}
+		if (plane_bbx[2](2) > 0.15 || plane_bbx[1](2) > 0.15) {
+			Eigen::Vector3f p2 = (plane_bbx[2] + plane_bbx[1]) * 0.5f;
+			points.push_back(p2);
+		}
+		if (plane_bbx[0](2) > 0.15 || plane_bbx[3](2) > 0.15) {
+			Eigen::Vector3f p3 = (plane_bbx[2] + plane_bbx[3]) * 0.5f;
+			points.push_back(p3);
+		}
+
+		for (int i = 0; i < points.size(); i++) {
+			Eigen::Vector3f v = plane_bbx[4] - points.at(i);
+			v = Eigen::Vector3f(v.x(), v.y(), 0);
+			points.at(i) -= _offset * v.normalized();
+			v.z() = 0;
+			vecs.push_back(v);
+		}
+		std::vector<geometry_msgs::Pose> poses;
+		for (int i = 0; i < points.size(); i++) {
+			geometry_msgs::Pose pose;
+			geometry_msgs::Point position = getRosPoint(points.at(i));
+			position.z = 0;
+			pose.position = position;
+			tf::Vector3 plane_norm(vecs.at(i).x(), vecs.at(i).y(),
+					vecs.at(i).z());
+			plane_norm = plane_norm.normalized();
+			tf::Quaternion qt = tf::shortestArcQuat(tf::Vector3(1, 0, 0),
+					plane_norm);
+			geometry_msgs::Quaternion robot_orientation;
+			tf::quaternionTFToMsg(qt, robot_orientation);
+			pose.orientation = robot_orientation;
+			poses.push_back(pose);
+		}
+		return poses;
 	}
 
-	for (int i = 0; i < points.size(); i++) {
-		Eigen::Vector3f v = plane_bbx[4] - points.at(i);
-		v = Eigen::Vector3f(v.x(), v.y(), 0);
-		points.at(i) -= _offset * v.normalized();
-		v.z() = 0;
-		vecs.push_back(v);
+	geometry_msgs::Point getRosPoint(Eigen::Vector3f point) {
+		geometry_msgs::Point p;
+		p.x = point(0);
+		p.y = point(1);
+		p.z = point(2);
+		return p;
 	}
-	std::vector<geometry_msgs::Pose> poses;
-	for (int i = 0; i < points.size(); i++) {
-		geometry_msgs::Pose pose;
-		geometry_msgs::Point position = getRosPoint(points.at(i));
-		position.z = 0;
-		pose.position = position;
-		tf::Vector3 plane_norm(vecs.at(i).x(), vecs.at(i).y(), vecs.at(i).z());
-		plane_norm = plane_norm.normalized();
-		tf::Quaternion qt = tf::shortestArcQuat(tf::Vector3(1, 0, 0),
-				plane_norm);
-		geometry_msgs::Quaternion robot_orientation;
-		tf::quaternionTFToMsg(qt, robot_orientation);
-		pose.orientation = robot_orientation;
-		poses.push_back(pose);
-	}
-	return poses;
-}
-
-geometry_msgs::Point getRosPoint(Eigen::Vector3f point) {
-	geometry_msgs::Point p;
-	p.x = point(0);
-	p.y = point(1);
-	p.z = point(2);
-	return p;
-}
 
 };
 
 int main(int argc, char** argv) {
 // Initialise ROS
-ros::init(argc, argv, "segmentation3d");
-ObjectDetection objDet;
+	ros::init(argc, argv, "segmentation3d");
+	ObjectDetection objDet;
 
 // Spin
-ros::spin();
+	ros::spin();
 
-return 0;
+	return 0;
 }
