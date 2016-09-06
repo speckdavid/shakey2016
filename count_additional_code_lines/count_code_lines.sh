f88979c708bc4c1285feb66ee199cddfa57effe8
David Speck <david.speck@pluto.uni-freiburg.de>
2016-09-07 01:36:23 +0200

    now all files are counted

--- count_additional_code_lines/count_code_lines.sh
+++ count_additional_code_lines/count_code_lines.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cloc --exclude-list-file=black_list_files --list-file=white_list_dirs ../
+printf "\n\nPlease start this script from the count_additional_code_lines folder\n\n"
+cloc --exclude-list-file=black_list_files --list-file=white_list_dirs --force-lang="LISP",pddl  --force-lang="XML",launch ../ | awk '{gsub("Lisp","PDDL"); print $0}'
