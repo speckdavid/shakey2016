1b44ba02724c44c1a28300a9615959dba5462d51
David Speck <david.speck@pluto.uni-freiburg.de>
2016-09-07 16:23:48 +0200

    made scipt independent of invoked folder

--- count_additional_code_lines/count_code_lines.sh
+++ count_additional_code_lines/count_code_lines.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-printf "\n\nPlease start this script from the count_additional_code_lines folder\n\n"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 cloc --exclude-list-file=black_list_files --list-file=white_list_dirs --force-lang="LISP",pddl  --force-lang="XML",launch ../ | awk '{gsub("Lisp","PDDL"); print $0}'
